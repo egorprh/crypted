c96a23293cfbfe26b05755f595ad0b1074e5e11c
EgorPrh <e.prikhodko@lms-service.ru>
2025-05-31 17:56:21 +0300

    build front and docker fix

--- frontend/dist/assets/LessonQuizTest-CTXg3ap-.js
+++ frontend/dist/assets/LessonQuizTest-CTXg3ap-.js
@@ -1,4 +1,4 @@
-import{r as u,d as F,a as J,b as Y,j as n}from"./index-BTBytZu6.js";/* empty css                         */import{A as K}from"./Alert-CZm2Osj6.js";/*! *****************************************************************************
+import{r as u,d as F,b as J,a as Y,j as n}from"./index-858hcOPc.js";/* empty css                         */import{A as K}from"./Alert-BQP8sPxV.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this file except in compliance with the License. You may obtain a copy of the
