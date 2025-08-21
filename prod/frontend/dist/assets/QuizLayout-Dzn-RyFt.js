2f5abbdd7493e66f1fd1ccb6aa5751fd407d745b
EgorPrh <e.prikhodko@lms-service.ru>
2025-08-21 17:47:12 +0300

    rm logs, prod and frontstable

--- prod/frontend/dist/assets/QuizLayout-Dzn-RyFt.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as i}from"./index-858hcOPc.js";/* empty css               */function a(){return t.jsx("div",{className:"container quiz-container",children:t.jsx("main",{children:t.jsx(i,{})})})}export{a as default};
