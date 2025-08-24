d40ff02d12582186b9392a3b28581bd09804e54f
EgorPrh <e.prikhodko@lms-service.ru>
2025-08-24 20:02:13 +0300

    front build

---
+++ frontend/dist/assets/Alert-BeGwKm9r.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CRjUz0L_.js";/* empty css              */function i({text:s}){return r.jsx("div",{className:"save-error",children:r.jsxs("p",{children:["⚠️ ",s]})})}export{i as A};
