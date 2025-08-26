a5cc12b89e59c86820ebe47f0f99eaede69baf39
EgorPrh <e.prikhodko@lms-service.ru>
2025-08-26 11:40:00 +0300

    build

---
+++ frontend/dist/assets/Alert-D3YjjDDq.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DkNEoGFQ.js";/* empty css              */function i({text:s}){return r.jsx("div",{className:"save-error",children:r.jsxs("p",{children:["⚠️ ",s]})})}export{i as A};
